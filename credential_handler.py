cc5966fdadf558d11cafcf74a10ea9993a131d9e
Brandon-Do <brandondo64@gmail.com>
2019-07-21 13:02:20 -0700

    Added configuration handler class

---
+++ credential_handler.py
@@ -0,0 +1,22 @@
+from configparser import ConfigParser
+
+class ConfigurationHandler:
+    def __init__(self, file_name='credentials.ini'):
+        self.config = {}
+        self.read_credentials_from_file(file_name)
+
+    def get_configuration_section(self, section_name):
+        return self.config[section_name]
+
+    def get_configuration_value(self, section_name, value_name):
+        return self.config[section_name][value_name]
+
+    def read_credentials_from_file(self, file_name):
+        try:
+            self.config = ConfigParser()
+            self.config.read(file_name)
+        except:
+            raise Exception('trying to read credentials from {} which does not exist.', file_name)
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import credential_handler as cred
+
 def main():
-    print('hello')
+    print('starting program')
+    creds = cred.ConfigurationHandler()
+    zwid = creds.get_configuration_value('ZILLOW', 'zwid')
+    api_url = creds.get_configuration_value ('ZILLOW', 'api_url')
+    print(zwid, api_url)
 
 if __name__ == '__main__':
     main()
